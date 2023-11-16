26f7f60606ea82a5c5e9db0e7c659ffae903f01a
Anael Jonas <anael.alencar@compasso.com.br>
2023-11-16 14:38:05 -0300

    feature: pre folders organizations and service example

---
+++ src/data/services/example-service.ts
@@ -0,0 +1,16 @@
+interface IExampleServiceInterface {
+    get(): void,
+    post(): void
+};
+
+const ExampleService: IExampleServiceInterface = {
+    get() {
+        // Exemplo de requisição.
+    },
+
+    post() {
+        // Exemplo de requisição.
+    }
+};
+
+export default ExampleService;
